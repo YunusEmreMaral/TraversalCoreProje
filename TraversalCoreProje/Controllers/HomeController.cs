98471e26634c4fcba59570d785eb1961f5015b27
Yunus Emre Maral <maralyunus41@gmail.com>
2024-03-09 01:24:31 +0300

    Genel düzenlemeler ve temel yapının kurulması

--- TraversalCoreProje/Controllers/HomeController.cs
+++ TraversalCoreProje/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace TraversalCoreProje.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
+		
+		public IActionResult Deneme()
+		{
+			return View();
+		}
 	}
 }
